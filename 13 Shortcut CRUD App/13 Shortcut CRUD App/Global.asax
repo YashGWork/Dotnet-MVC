0530bc2feb25b6ba4149a25f4f8541c3e74b0422
Yash Gupta <yashg.workmail@gmail.com>
2024-12-02 11:51:29 +0530

    Second Commit

---
+++ 13 Shortcut CRUD App/13 Shortcut CRUD App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_13_Shortcut_CRUD_App.MvcApplication" Language="C#" %>
---
+++ 13 Shortcut CRUD App/13 Shortcut CRUD App/ShortcutCRUDApp.cs
@@ -0,0 +1,18 @@
+﻿/*
+Shortcut CRUD App
+
+To create this shortcut CRUD App we will use Entity Framework and Database First Approach (Data should be present in the database).
+
+-> Steps
+
+1) Make sure the data that is to be referred is present in the database sql server management studio.
+
+1) Install Entity Framework package from Nuget Package Manager.
+
+3) Create Model class using Database First Approach using ADO.NET and select the database to be referred to.
+
+4) Add MVC5 Controller with views, using Entity Framework and select the Model class and Data context class to refer to. (This will automatically create the controller's logic and corresponding CRUD views)'
+
+5) Edit Route config file and change the default controller to emps controller
+
+*/
